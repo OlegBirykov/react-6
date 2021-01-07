dc635e6a6b7ebe5ae6e271a0a8ef828be3d020d2
OlegBirykov <p_petro75@mail.ru>
2021-01-07 19:57:28 +0300

    Задание №2 выполнено

--- src/task2/Note.js
+++ src/task2/Note.js
@@ -1,18 +1,18 @@
 import PropTypes from 'prop-types';
 
 function Note(props) {
-  const { text, onButtonClick } = props;
+  const { content, onButtonClick } = props;
 
   return (
     <div className="Note">
-      <p className="Note-text">{text}</p>
+      <p className="Note-text">{content}</p>
       <button className="Note-button-delete" type="button" onClick={onButtonClick}>&#x274C;</button>
     </div>
   );
 }
 
 Note.propTypes = {
-  text: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
   onButtonClick: PropTypes.func.isRequired
 };
 
--- src/task2/Notes.js
+++ src/task2/Notes.js
@@ -7,19 +7,59 @@ class Notes extends React.Component {
   }
 
   componentDidMount() {
-
+    this.updateNotes();
   }
 
-  addNote() {
+  async addNote(event) {
+    event.preventDefault();
 
+    const note = event.target.note;
+    note.value = note.value.trim();
+    if(!note.value) {
+      return;
+    }
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      body: JSON.stringify({
+        id: 0,
+        content: note.value
+      })
+    }
+
+    event.target.note.value = '';
+    
+    const response = await fetch(process.env.REACT_APP_NOTES_URL, options);
+    if (response.status !== 204) {
+      alert('Ошибка связи с сервером');
+      return;
+    }
+    this.updateNotes();
   }
 
-  updateNotes() {
-
+  async updateNotes() {
+    const response = await fetch(process.env.REACT_APP_NOTES_URL); 
+    if (response.status !== 200) {
+      alert('Ошибка связи с сервером');
+      return;
+    }
+    this.setState({ notes: await response.json() }); 
   }
 
-  deleteNote(id) {
+  async deleteNote(id) {
+    const options = {
+      method: 'DELETE',
+    }
 
+    const response = await fetch(process.env.REACT_APP_NOTES_URL + '/' + id, options);
+    if (response.status !== 204) {
+      alert('Ошибка связи с сервером');
+      return;
+    }
+    this.updateNotes();
   }
 
   render() {
@@ -27,14 +67,14 @@ class Notes extends React.Component {
       <div className="Notes">  
         <div className="Notes-title">
           <h2 className="Notes-title-text">Notes</h2>
-          <button className="Notes-button-update" type="button">&#x1F503;</button>
+          <button className="Notes-button-update" type="button" onClick={() => this.updateNotes()}>&#x1F503;</button>
         </div>
         <div className="Notes-notes">
-          {this.state.notes.map(({ id, text }) => 
-            <Note text={text} onButtonClick={() => this.deleteNote(id)} key={id} />
+          {this.state.notes.map(({ id, content }) => 
+            <Note content={content} onButtonClick={() => this.deleteNote(id)} key={id} />
           )}
         </div>
-        <form className="Notes-form">
+        <form className="Notes-form" onSubmit={(event) => this.addNote(event)}>
           <label className="Notes-form-label">
             New notes
             <textarea className="Notes-form-input" rows="8" name="note" />
